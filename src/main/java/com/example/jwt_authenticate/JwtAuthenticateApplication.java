a76fbf0fb3d430d6a5ee7fe0c2093e081d184830
Alok <alokthakur609@gmail.com>
2026-02-09 13:37:58 +0530

    initial commit

---
+++ src/main/java/com/example/jwt_authenticate/JwtAuthenticateApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwt_authenticate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthenticateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAuthenticateApplication.class, args);
+	}
+
+}
